49d869b0cfb83fda4b2013122049368b5fe996d5
cansevertaner <taner.cnsvr@gmail.com>
2018-12-27 11:58:54 +0300

    Django Girls uygulamam,new commit123412

--- oyunsite/settings.py
+++ oyunsite/settings.py
@@ -126,4 +126,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_ROOT = '/static/'
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
